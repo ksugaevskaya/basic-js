572bb0f52ea0100b88d23ba707ee1c312238525a
ksugaevskaya <ksu.gaevskaya@gmail.com>
2024-12-27 01:49:11 +0300

    feat: completed 3 new tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,58 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+ * => 'STRING PLUS 00 PLUS 00 PLUS ** STRING PLUS00 PLUS00 PLUS ** STRING PLUS00 PLUS00 PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = "";
+  if (options.separator === undefined) {
+    options.separator = "+";
+  }
+
+  let addition = "";
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = "|";
+  }
+  if (
+    options.additionRepeatTimes === undefined &&
+    options.addition !== undefined
+  ) {
+    options.additionRepeatTimes = 1;
+  }
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+
+  for (let k = 0; k < options.additionRepeatTimes; k++) {
+    if (k < options.additionRepeatTimes - 1) {
+      addition += options.addition + options.additionSeparator;
+    } else {
+      addition += options.addition;
+    }
+  }
+
+  for (i = 0; i < options.repeatTimes; i++) {
+    if (i < options.repeatTimes - 1) {
+      result += str + addition + options.separator;
+    } else {
+      result += str + addition;
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
